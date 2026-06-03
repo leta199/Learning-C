4f2ecdc9d9eca9b0ff7b50ab26c71a368025c558
“leta199” <215572592+leta199@users.noreply.github.com>
2026-06-03 16:05:52 -0400

    Getting started is looking good

--- Level 0/getting_started.c
+++ Level 0/getting_started.c
@@ -3,3 +3,21 @@
 #include <stdlib.h>
 
 /* Main function */
+int main (int argc, [[maybe_unused]] char* argv[argc+1]){
+    // Decalrations for the variables A elements
+    double A[5]= {
+        [0] = 2.0,
+        [1] = 4.0,
+        [4] = 3.E+25,
+        [3] = 0.0007,
+
+    };
+// Doing work on the variable A
+for (size_t  i = 0; i < 5; ++i){
+    printf("Element %zu is %g, \tits square is %g\n",
+    i,
+    A[i],
+    A[i]*A[i]);
+}
+return EXIT_SUCCESS;
+}
