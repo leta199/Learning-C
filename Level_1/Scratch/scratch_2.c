3933e7eb5b758c6f004a3c4e9649b61cd62b54aa
“leta199” <215572592+leta199@users.noreply.github.com>
2026-06-06 13:34:42 -0400

    Conditional execution

---
+++ Level_1/Scratch/scratch_2.c
@@ -0,0 +1,11 @@
+// Conditional statements 
+
+#include <stdio.h>
+int main(void){
+ 
+ for (size_t i = 10 ; i >= 7; --i){
+ if (i){ /*Perform printf function if True i.e i =/= 0 */
+    printf("Our value of i is %zu.\n",
+    i);
+ }}
+}
