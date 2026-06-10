0ff4d72ea09edd988dfb05acbcbb003cf886715a
“leta199” <215572592+leta199@users.noreply.github.com>
2026-06-10 19:30:44 -0400

    Added return statement to Heron function

--- Level_1/Scratch/scratch_4.c
+++ Level_1/Scratch/scratch_4.c
@@ -2,18 +2,21 @@
 
 #include <tgmath.h>
 #include <stdio.h>
+#include <stdlib.h>
 
 int main(void) {
 
+// Initialised variables 
 double  eps = 1E-9;// level of precision 
-double const a = 0.01;
-double x = 0.5;
-int counter = 0;
+double const a = 0.01; // denomiantor
+double x = 0.5; // random guess to start 
+int counter = 0; // track number of iterations 
 
-while ( fabs(1.0 - a*x) >= eps) {
+while ( fabs(1.0 - a*x) >= eps) { // tracks whether or not 1- a*x is greater than or equal to eps
 x *= (2.0 - a*x); 
-printf("The value of 1/x is: %f, on iteration: %d.\n", x, counter);
-++ counter;
-}}
+printf("The value of 1/a is: %f, on iteration: %d.\n", x, counter);
+++ counter; // iterate on our counter 
+}
+return EXIT_SUCCESS; }
 
 
