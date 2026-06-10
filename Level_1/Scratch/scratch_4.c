82beb87985bcc105ec280b0583a128a3e99606fd
“leta199” <215572592+leta199@users.noreply.github.com>
2026-06-10 19:26:34 -0400

    Fixed the Heron function

--- Level_1/Scratch/scratch_4.c
+++ Level_1/Scratch/scratch_4.c
@@ -5,14 +5,14 @@
 
 int main(void) {
 
-double  F = 1E-9;// level of precision 
-double const a = 34.0;
+double  eps = 1E-9;// level of precision 
+double const a = 0.01;
 double x = 0.5;
 int counter = 0;
 
-while ( fabs(1.0 - a*x) >= F) {
+while ( fabs(1.0 - a*x) >= eps) {
 x *= (2.0 - a*x); 
-printf("The value of x is: %f, on iteration: %d.\n", x, counter);
+printf("The value of 1/x is: %f, on iteration: %d.\n", x, counter);
 ++ counter;
 }}
 
