c345ada8545c762e67bda17af13d1729fc1d98fb
“leta199” <215572592+leta199@users.noreply.github.com>
2026-06-11 18:51:47 -0400

    Started with challenge 1

--- Level_1/Amateur_exercises/Chl_1_merge_sort.c
+++ Level_1/Amateur_exercises/Chl_1_merge_sort.c
@@ -1,2 +1,20 @@
 // CHALLENGE 1 
 // Implement merge sort (with tests)
+
+#include <stdio.h>
+int main(void){
+// Declare and define "A" array 
+int A[5] = {
+    [0] = 3,
+    [1] = 4,
+    [2] = 9,
+    [3] = 10,
+    [4] = 5,
+
+};
+
+// Spliting logic
+size_t n = sizeof(A)/sizeof(int);
+printf("Length of array is %s.", n);
+
+}
