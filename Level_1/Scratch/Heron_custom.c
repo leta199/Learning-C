1d8af0346ff8f79c25450c6063797ccbb8ec3c40
“leta199” <215572592+leta199@users.noreply.github.com>
2026-06-14 21:01:56 -0400

    Added heron_Custom to gitignore

--- Level_1/Scratch/Heron_custom.c
+++ Level_1/Scratch/Heron_custom.c
@@ -8,7 +8,7 @@ constantexpr; double  eps1p24 = 1.0 + 0x1P-24;
 
 int main( int argc, char* argv[argc+1]){
  for ( int i = 1; i < argc; ++i){
-    double const a = strtod(argv[i], nullptr);
+    double const a = strtod(argv[i], NULL);
     double x = 1.0;
     for (;;){
         double prod = a*x;
