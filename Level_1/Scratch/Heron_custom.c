1bb15afba3b4c74c50a3eac2237e41b6d13bf37a
“leta199” <215572592+leta199@users.noreply.github.com>
2026-06-14 21:11:56 -0400

    Heron approx textbook done

--- Level_1/Scratch/Heron_custom.c
+++ Level_1/Scratch/Heron_custom.c
@@ -1,10 +1,10 @@
 #include <stdio.h>
 #include <stdlib.h>
 
-constantexpr; double  eps1m01 = 1.0 - 0x1P-01;
-constantexpr; double  eps1p01 = 1.0 + 0x1P-01;
-constantexpr; double  eps1m24 = 1.0 - 0x1P-24;
-constantexpr; double  eps1p24 = 1.0 + 0x1P-24;
+double  eps1m01 = 1.0 - 0x1P-01;
+double  eps1p01 = 1.0 + 0x1P-01;
+double  eps1m24 = 1.0 - 0x1P-24;
+double  eps1p24 = 1.0 + 0x1P-24;
 
 int main( int argc, char* argv[argc+1]){
  for ( int i = 1; i < argc; ++i){
@@ -29,7 +29,7 @@ int main( int argc, char* argv[argc+1]){
             break;
         }
     }
-    printf("heorn: a = %,5e, \t x = %.5e, \ta*x = %.12f/n",
+    printf("heron: a = %.5e, \t x = %.5e, \ta*x = %.12f\n",
     a,x,a*x);
  }
 return EXIT_SUCCESS;
