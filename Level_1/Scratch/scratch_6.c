0a58c2fca0ea4de5aeed5c4106af4a5c02778120
“leta199” <215572592+leta199@users.noreply.github.com>
2026-06-15 20:56:39 -0400

    Finished scratch_6.c

--- Level_1/Scratch/scratch_6.c
+++ Level_1/Scratch/scratch_6.c
@@ -1,1 +1,10 @@
 // Arithemtic in C
+#include <stdio.h>
+
+int main(int argc, char* argv[]){
+    size_t a = argv[1][0];
+    size_t b = argv[2][0];
+    size_t c = (a + b)*a;
+    printf("Your answer is: c",c);
+
+}
