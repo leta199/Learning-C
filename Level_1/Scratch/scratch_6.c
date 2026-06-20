5604ae07e55475d2f645529694057d1ff8058f52
“leta199” <215572592+leta199@users.noreply.github.com>
2026-06-20 19:53:57 -0400

    We're redoing scratch 6 with K.N King book scanf

--- Level_1/Scratch/scratch_6.c
+++ Level_1/Scratch/scratch_6.c
@@ -3,9 +3,5 @@
 #include <stdlib.h>
 
 int main( void){
-    size_t a = 3;
-    size_t b = 6;
-    size_t c = (a + b)*a;
-    printf("A is %zu \tB is %zu. \nYour answer is: %zu.\n",a,b,c);
-    return EXIT_SUCCESS;
+
 }
