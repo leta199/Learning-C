dc56cfb9252b45d280853684bdb2cf78c37a1969
“leta199” <215572592+leta199@users.noreply.github.com>
2026-06-20 19:59:25 -0400

    Scratch 6 scanf

--- Level_1/Scratch/scratch_6.c
+++ Level_1/Scratch/scratch_6.c
@@ -1,7 +1,11 @@
 // Arithemtic in C
 #include <stdio.h>
-#include <stdlib.h>
 
 int main( void){
-
+    int weight, height ;
+    printf("Enter you weight");
+    scanf("%f", &weight);
+    printf("Enter you height");
+    scanf("%f", &weight);
+    printf("Your BMI is: %.2f\n", weight/ height);
 }
