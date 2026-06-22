a5c7779722091c6deed6a6b42a4618d427ef4a34
“leta199” <215572592+leta199@users.noreply.github.com>
2026-06-21 22:29:23 -0400

    Oops

---
+++ Main_f/Scratch/scratch_4.c
@@ -0,0 +1,22 @@
+// While loops - Heron approximation
+
+#include <tgmath.h>
+#include <stdio.h>
+#include <stdlib.h>
+
+int main(void) {
+
+// Initialised variables 
+double  eps = 1E-9;// level of precision 
+double const a = 0.01; // denomiantor
+double x = 0.5; // random guess to start 
+int counter = 0; // track number of iterations 
+
+while ( fabs(1.0 - a*x) >= eps) { // tracks whether or not 1- a*x is greater than or equal to eps
+x *= (2.0 - a*x); // Newton Raphson iteration method x*(2 - a*x)
+printf("The value of 1/a is: %f, on iteration: %d.\n", x, counter);
+++ counter; // iterate on our counter 
+}
+return EXIT_SUCCESS; }
+
+
