12e30df0c2873b92b1ac5514899e4473a3a6c24f
“leta199” <215572592+leta199@users.noreply.github.com>
2026-06-23 19:06:41 -0400

    Updates to project 1

--- Main_f/Projects/project_1.c
+++ Main_f/Projects/project_1.c
@@ -1,8 +1,15 @@
-// What is the volume of that sphere 
+// What is the volume of that sphere? 
 #include <stdio.h>
 int main(void)
 {
+// declarations that we will store
     float radius;
-
     
+// input values
+    printf("Please enter the radius of your circle (in cm)");
+    scanf("%f", & radius);
+
+// calucation and display 
+    printf("Your circle is of volume: ")
+
 }
