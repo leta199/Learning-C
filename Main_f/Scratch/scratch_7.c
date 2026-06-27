3c00e5b3329c7dbb773f93c6833c9b2066c52bba
“leta199” <215572592+leta199@users.noreply.github.com>
2026-06-26 23:23:13 -0400

    Comments for conversion specifiers

--- Main_f/Scratch/scratch_7.c
+++ Main_f/Scratch/scratch_7.c
@@ -7,8 +7,8 @@ int main(void)
 //declarations 
     int a = 12;
     float b = 12487.35986f;
-// print displays 
-    printf("|%d||%5d||%-5d||%5.3d|\n",a,a,a,a);
+// print displays - conversion specifiers 
+    printf("|%d||%5d||%-5d||%5.3d|\n",a,a,a,a); 
     printf("|%f||%5.6f||%5.6g||%5.6e|\n",b,b,b,b);
 
     return 0;
