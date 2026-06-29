c11d61ab31c2384f22ddd52a749ed835fee7a43a
“leta199” <215572592+leta199@users.noreply.github.com>
2026-06-28 20:17:55 -0400

    Project 2 scanf

--- Main_f/Projects/project_2.c
+++ Main_f/Projects/project_2.c
@@ -5,6 +5,10 @@
 int main(void) 
 {
 int arg1, arg2, arg3, arg4, arg5, arg6, arg7, arg8, arg9, 
-arg10, arg11, arg12, arg13, arg14, arg15, arg16    
+arg10, arg11, arg12, arg13, arg14, arg15, arg16;    
 
+// Printing, taking in and storing variables
+printf("Please enter your sixteen digits\n");
+scanf("%d %d %d %d %d %d %d %d %d %d %d %d %d %d %d %d", 
+)
 }
