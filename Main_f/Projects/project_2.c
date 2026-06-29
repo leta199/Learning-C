2be0203ad282514f147460838ecfd99079f044a5
“leta199” <215572592+leta199@users.noreply.github.com>
2026-06-28 20:33:52 -0400

    Done with the base input output

--- Main_f/Projects/project_2.c
+++ Main_f/Projects/project_2.c
@@ -5,10 +5,15 @@
 int main(void) 
 {
 int arg1, arg2, arg3, arg4, arg5, arg6, arg7, arg8, arg9, 
-arg10, arg11, arg12, arg13, arg14, arg15, arg16;    
+    arg10, arg11, arg12, arg13, arg14, arg15, arg16;    
 
 // Printing, taking in and storing variables
-printf("Please enter your sixteen digits\n");
+printf("Please enter your sixteen digits: ");
 scanf("%d %d %d %d %d %d %d %d %d %d %d %d %d %d %d %d", 
-)
+     &arg1, &arg2, &arg3, &arg4, &arg5, &arg6, &arg7, &arg8, &arg9, 
+     &arg10, &arg11, &arg12, &arg13, &arg14, &arg15, &arg16);
+printf("You have entered \n\t%d %d %d %d\n\t%d %d %d %d\n\t%d %d %d %d\n\t%d %d %d %d\n", 
+     arg1, arg2, arg3, arg4, arg5, arg6, arg7, arg8, arg9, 
+     arg10, arg11, arg12, arg13, arg14, arg15, arg16);
+return 0;
 }
