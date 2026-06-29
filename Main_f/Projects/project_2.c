eb150771c3aff4554007adb8943af1f70a417925
“leta199” <215572592+leta199@users.noreply.github.com>
2026-06-28 20:43:36 -0400

    Input output for proecjt 2 done

--- Main_f/Projects/project_2.c
+++ Main_f/Projects/project_2.c
@@ -8,12 +8,13 @@ int arg1, arg2, arg3, arg4, arg5, arg6, arg7, arg8, arg9,
     arg10, arg11, arg12, arg13, arg14, arg15, arg16;    
 
 // Printing, taking in and storing variables
-printf("Please enter your sixteen digits: ");
-scanf("%d %d %d %d %d %d %d %d %d %d %d %d %d %d %d %d", 
+printf("Please enter your sixteen numbers e.g ( 1 2 3): ");
+scanf("%d%d%d%d%d%d%d%d%d%d%d%d%d%d%d%d", 
      &arg1, &arg2, &arg3, &arg4, &arg5, &arg6, &arg7, &arg8, &arg9, 
      &arg10, &arg11, &arg12, &arg13, &arg14, &arg15, &arg16);
-printf("You have entered \n\t%d %d %d %d\n\t%d %d %d %d\n\t%d %d %d %d\n\t%d %d %d %d\n", 
+printf("You have entered: \n\t%3d %3d %3d %3d\n\t%3d %3d %3d %3d\n\t%3d %3d %3d %3d\n\t%3d %3d %3d %3d\n", 
      arg1, arg2, arg3, arg4, arg5, arg6, arg7, arg8, arg9, 
      arg10, arg11, arg12, arg13, arg14, arg15, arg16);
+
 return 0;
 }
