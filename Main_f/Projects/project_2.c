013f26d4d1086bd0f7f12e29cf690cf0848c923a
“leta199” <215572592+leta199@users.noreply.github.com>
2026-06-29 20:40:08 -0400

    We finished the basic sums

--- Main_f/Projects/project_2.c
+++ Main_f/Projects/project_2.c
@@ -15,7 +15,17 @@ scanf("%d%d%d%d%d%d%d%d%d%d%d%d%d%d%d%d",
 printf("You have entered: \n\t%3d %3d %3d %3d\n\t%3d %3d %3d %3d\n\t%3d %3d %3d %3d\n\t%3d %3d %3d %3d\n", 
      arg1, arg2, arg3, arg4, arg5, arg6, arg7, arg8, arg9, 
      arg10, arg11, arg12, arg13, arg14, arg15, arg16);
-printf("The sum of each row is:\t%d\t%d\t%d", arg1 + arg2 + arg3 +arg4, 
-    arg5 + arg6 + arg7 + arg8,  arg9 + arg10 + arg11 + arg12);
+
+// Printing row, column and diagonal sums 
+printf("Row sums:\t%d\t%d\t%d", arg1 + arg2 + arg3 +arg4, 
+                                              arg5 + arg6 + arg7 + arg8,  
+                                              arg9 + arg10 + arg11 + arg12,
+                                              arg13 + arg14 + arg15 + arg16);
+printf("Column sums:\t%d\t%d\t%d", arg1 + arg5 + arg9 +arg13, 
+                                              arg2 + arg6 + arg10 + arg14,  
+                                              arg3 + arg7 + arg11 + arg15,
+                                              arg4 + arg8 + arg12 + arg16);
+printf("Diagonal sums:\t%d\t%d", arg1 + arg6 + arg11 +arg15, 
+                                              arg4 + arg7 + arg10 + arg13);                                                                                           
 return 0;
 }
