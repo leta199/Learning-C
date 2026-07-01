3b2af211504083bf41cb2120827a1a90f56a8861
“leta199” <215572592+leta199@users.noreply.github.com>
2026-07-01 19:15:27 -0400

    Added print functionality

--- Main_f/Scratch/scratch_9.c
+++ Main_f/Scratch/scratch_9.c
@@ -8,12 +8,14 @@ int main(void)
     int numerator1, numerator2, denominator1, denominator2, new_denominator, new_numerator;
 
 // calculations 
-    printf("Please enter what fractions you want to add:\n");
+    printf("Please enter what fractions you want to add: ");
     scanf("%d /%d +%d /%d", &numerator1, &numerator2, &denominator1, &denominator2);
     
     new_denominator = denominator1 * denominator2;
     new_numerator = numerator1 * denominator2 + numerator2 * denominator1;
-
+// final print 
+    printf("The result is: %d/%d", new_numerator, new_denominator);
+    
 return 0;
 
 }
