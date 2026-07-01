415b016b04f186449634b6111462ed05cd8c418e
“leta199” <215572592+leta199@users.noreply.github.com>
2026-07-01 19:01:31 -0400

    Working with fractions

---
+++ Main_f/Scratch/scratch_9.c
@@ -0,0 +1,14 @@
+// How to use fractions in standard I/O 
+
+#include <stdio.h>
+
+int main(void)
+{
+// declarations 
+    int fraction1, fraction2;
+// calculations 
+    printf("Please enter what fractions you want to add:\n");
+    scanf("%d/%d", &fraction1, &fraction2);
+
+return 0;
+}
