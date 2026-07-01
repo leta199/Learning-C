1be250cfaffb7249f1951b6cad23cb8ff361ba43
“leta199” <215572592+leta199@users.noreply.github.com>
2026-07-01 19:09:13 -0400

    Implementing calculation logic

--- Main_f/Scratch/scratch_9.c
+++ Main_f/Scratch/scratch_9.c
@@ -5,10 +5,14 @@
 int main(void)
 {
 // declarations 
-    int fraction1, fraction2;
+    int numerator1, numerator2, denominator1, denominator2;
+
 // calculations 
     printf("Please enter what fractions you want to add:\n");
-    scanf("%d/%d", &fraction1, &fraction2);
+    scanf("%d /%d +%d /%d", &numerator1, &numerator2, &denominator1, &denominator2);
+    
+    int new_denominator denominator1*denominator2;
+    
 
 return 0;
 }
