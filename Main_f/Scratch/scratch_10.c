6d8a4d8ca5f313314c567690d8b301aba489088d
“leta199” <215572592+leta199@users.noreply.github.com>
2026-07-02 22:22:28 -0400

    Final comments

--- Main_f/Scratch/scratch_10.c
+++ Main_f/Scratch/scratch_10.c
@@ -4,12 +4,12 @@
 
 int main(void)
 {
-// Declarations with prefix increment
+// Declarations and prints with prefix increment
     int i = 1;
     printf("i is: %d\n",++i); /*side effect - increments i immediately - i is now 2*/
     printf("i is: %d\n",i); /*i stays 2*/
 
-// Declarations with postfix increment
+// Declarations and prints with postfix increment
     int j = 2;
     printf("j is: %d\n",j++); /*does not increment j immediately- j is still 2 */
     printf("j is: %d\n",j); /*side effect - j is now 3*/
