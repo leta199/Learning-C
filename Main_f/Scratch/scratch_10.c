c0fdc48a8b0547d975081199214013e2b1416a44
“leta199” <215572592+leta199@users.noreply.github.com>
2026-07-02 22:09:23 -0400

    Operations in C scratch 10

---
+++ Main_f/Scratch/scratch_10.c
@@ -0,0 +1,16 @@
+// Operations in C
+
+#include <stdio.h>
+
+int main(void)
+{
+    int i = 1;
+    printf("i is: %d.\n",++i);
+    printf("i is: %d",++i);
+
+    int j = 2;
+    printf("j is: %d.\n",j++);
+    printf("j is: %d",j++);
+
+return 0;
+}
