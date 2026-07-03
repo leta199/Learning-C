8868659303e2b9c9bc85d499d779d4657e7cb65f
“leta199” <215572592+leta199@users.noreply.github.com>
2026-07-02 22:20:36 -0400

    More comments

--- Main_f/Scratch/scratch_10.c
+++ Main_f/Scratch/scratch_10.c
@@ -4,13 +4,15 @@
 
 int main(void)
 {
+// Declarations with prefix increment
     int i = 1;
     printf("i is: %d\n",++i); /*side effect - increments i immediately - i is now 2*/
     printf("i is: %d\n",i); /*i stays 2*/
 
+// Declarations with postfix increment
     int j = 2;
     printf("j is: %d\n",j++); /*does not increment j immediately- j is still 2 */
-    printf("j is: %d\n",j); /*side effect- j is now 3*/
+    printf("j is: %d\n",j); /*side effect - j is now 3*/
 
 return 0;
 }
