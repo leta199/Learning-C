be0692b5e5614feb04590f0ee983e849641c982c
“leta199” <215572592+leta199@users.noreply.github.com>
2026-07-02 22:14:28 -0400

    Added comments for clarity

--- Main_f/Scratch/scratch_10.c
+++ Main_f/Scratch/scratch_10.c
@@ -5,12 +5,12 @@
 int main(void)
 {
     int i = 1;
-    printf("i is: %d\n",++i);
-    printf("i is: %d\n",i);
+    printf("i is: %d\n",++i); /*increments i immediately - i is now 2*/
+    printf("i is: %d\n",i); /*i is stays 2*/
 
     int j = 2;
-    printf("j is: %d\n",j++);
-    printf("j is: %d\n",j);
+    printf("j is: %d\n",j++); /*does not increment j immediately- j is still 2 */
+    printf("j is: %d\n",j); /*side effect- j is now 3*/
 
 return 0;
 }
