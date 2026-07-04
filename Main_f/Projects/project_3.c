622c04e427cd05319e6faafb45e182f40e209245
“leta199” <215572592+leta199@users.noreply.github.com>
2026-07-03 21:15:11 -0400

    Project 3 pass 2

--- Main_f/Projects/project_3.c
+++ Main_f/Projects/project_3.c
@@ -10,8 +10,8 @@ int digit1, digit2, digit3, digit4, digit5, digit6, digit7,
 
 // Requesting necessary 10 digits 
 printf("Please etner your 11 digits: ");
-scanf("%d%d%d%d%d%d%d%d%d%d%d", digit1, digit2, digit3, digit4, digit5, digit6, digit7,
-          digit8, digit9, digit10, digit11);
+scanf("%d%d%d%d%d%d%d%d%d%d%d", &digit1, &digit2, &digit3, &digit4, &digit5, &digit6, &digit7,
+          &digit8, &digit9, &digit10, &digit11);
 
 //Do required calculations 
 int firstsum = digit1 + digit3 + digit5 + digit7 + digit9 + digit11;
