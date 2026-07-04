d067144b4593b9b7224d1060b265a31d4b2d88e1
“leta199” <215572592+leta199@users.noreply.github.com>
2026-07-03 21:02:32 -0400

    Updating project_3

--- Main_f/Projects/project_3.c
+++ Main_f/Projects/project_3.c
@@ -2,6 +2,10 @@
 
 int main(void)
 {
+// Declaring all variables required 
+int digit1, digit2, digit3, digit4, digit5, digit6, digit7,
+    digit8, digit9, digit10, digit11;
 
-    
+// Requesting necessary 10 digits 
+
 }
