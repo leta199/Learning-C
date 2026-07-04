ba386c61cea8a0d7b519aa4f32abe7cba2177c30
“leta199” <215572592+leta199@users.noreply.github.com>
2026-07-03 21:09:38 -0400

    Starting calculations

--- Main_f/Projects/project_3.c
+++ Main_f/Projects/project_3.c
@@ -13,4 +13,7 @@ printf("Please etner your 11 digits: ");
 scanf("%d%d%d%d%d%d%d%d%d%d%d", digit1, digit2, digit3, digit4, digit5, digit6, digit7,
           digit8, digit9, digit10, digit11);
 
+//Do required calculations 
+int firstsum = digit1 + digit3 + digit5 + digit7 + digit9 + digit11;
+
 }
