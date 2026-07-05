00ea3d8ead982a983148cd85e5587c925588d23c
“leta199” <215572592+leta199@users.noreply.github.com>
2026-07-04 22:37:00 -0400

    More comments in scratch 11

--- Main_f/Scratch/scratch_11.c
+++ Main_f/Scratch/scratch_11.c
@@ -8,6 +8,8 @@ int main(void)
 float a;
 printf("What float value are you interested in: ");
 scanf("%f", &a);
+
+// Determining value of user input 
 if (a < 0)
     printf("The value of your float is < 0\n");
 else if (a == 0)
