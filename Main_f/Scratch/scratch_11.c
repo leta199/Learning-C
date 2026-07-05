29aa726a7761807a2af80875842a161ad22d5b86
“leta199” <215572592+leta199@users.noreply.github.com>
2026-07-04 22:35:34 -0400

    Changed print in scratch 11

--- Main_f/Scratch/scratch_11.c
+++ Main_f/Scratch/scratch_11.c
@@ -8,11 +8,11 @@ float a;
 printf("What float value are you interested in: ");
 scanf("%f", &a);
 if (a < 0)
-    printf("The value of a is < 0\n");
+    printf("The value of your float is < 0\n");
 else if (a == 0)
-    printf("The value of a is = 0\n");
+    printf("The value of your float is = 0\n");
 else 
-    printf("The value of a is > 0\n");
+    printf("The value of your float is > 0\n");
 
 return 0;
 }
