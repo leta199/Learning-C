3945011c6463a3c3095b50411d22b42d9a7fea28
“leta199” <215572592+leta199@users.noreply.github.com>
2026-07-04 22:28:51 -0400

    Finished scratch

--- Main_f/Scratch/scratch_11.c
+++ Main_f/Scratch/scratch_11.c
@@ -1,1 +1,16 @@
 // Selection statements - Cascaded if statments 
+
+#include <stdio.h>
+
+int main(void)
+{
+int a = 2;
+if (a < 0)
+printf("The value of a is < 0\n");
+else if (a == 0)
+printf("The value of a is = 0\n");
+else 
+printf("The value of a is > 0");
+
+return 0;
+}
