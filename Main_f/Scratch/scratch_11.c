4db550c0ac2fea23dc1d2fb7d9bf0391a8d1416e
“leta199” <215572592+leta199@users.noreply.github.com>
2026-07-04 22:32:03 -0400

    Added scanf to scratch 11

--- Main_f/Scratch/scratch_11.c
+++ Main_f/Scratch/scratch_11.c
@@ -4,13 +4,15 @@
 
 int main(void)
 {
-int a = 2;
+float a;
+printf("What float value are you interested in: ");
+scanf("%f", &a);
 if (a < 0)
     printf("The value of a is < 0\n");
 else if (a == 0)
     printf("The value of a is = 0\n");
 else 
-    printf("The value of a is > 0");
+    printf("The value of a is > 0\n");
 
 return 0;
 }
