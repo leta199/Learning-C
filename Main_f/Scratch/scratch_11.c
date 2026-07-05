f7623537c01e076e33189226c52bdd3f8f81b8a5
“leta199” <215572592+leta199@users.noreply.github.com>
2026-07-04 22:29:28 -0400

    Indented print statements

--- Main_f/Scratch/scratch_11.c
+++ Main_f/Scratch/scratch_11.c
@@ -6,11 +6,11 @@ int main(void)
 {
 int a = 2;
 if (a < 0)
-printf("The value of a is < 0\n");
+    printf("The value of a is < 0\n");
 else if (a == 0)
-printf("The value of a is = 0\n");
+    printf("The value of a is = 0\n");
 else 
-printf("The value of a is > 0");
+    printf("The value of a is > 0");
 
 return 0;
 }
