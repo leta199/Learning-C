cd8c5662f45636e2a837cdf9573d8eeb7fb00ee3
“leta199” <215572592+leta199@users.noreply.github.com>
2026-07-11 11:46:53 -0400

    Finished with scratch layout

--- Main_f/Scratch/scratch_12.c
+++ Main_f/Scratch/scratch_12.c
@@ -1,1 +1,16 @@
 // Conditional expressions - the same as math conditionals "if P, then Q"
+
+#include <stdio.h>
+
+int main(void)
+{
+int i,j,k;
+printf("What is your value of i and j:");
+scanf("%d%d", &i, &j);
+
+k = (i > j) ? i+j : 0;
+
+printf("Your value of k is: %d", k);
+
+return 0;
+}
