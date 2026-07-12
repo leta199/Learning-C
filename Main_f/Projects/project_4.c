f1157a47c2f7819ca45efa03c83e2e12a453c627
“leta199” <215572592+leta199@users.noreply.github.com>
2026-07-12 19:50:41 -0400

    Started the begins of tax calculator

---
+++ Main_f/Projects/project_4.c
@@ -0,0 +1,14 @@
+// Tax Calculator - Federal income tax rate USA
+
+#include <stdio.h>
+#
+int main(void) 
+{
+
+float cash_income;
+printf("What is your total compensation in US$: ");
+scanf("%f", &cash_income);
+
+
+
+}
