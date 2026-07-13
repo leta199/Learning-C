9f59df3b0293006f3c34992d6fba9204c6e59284
“leta199” <215572592+leta199@users.noreply.github.com>
2026-07-12 20:00:57 -0400

    Implementing tax rate logic

--- Main_f/Projects/project_4.c
+++ Main_f/Projects/project_4.c
@@ -1,7 +1,14 @@
 // Tax Calculator - Federal income tax rate USA
 
 #include <stdio.h>
-#
+#define TAXRATE_1 0.1
+#define TAXRATE_2 0.12
+#define TAXRATE_3 0.22
+#define TAXRATE_4 0.24
+#define TAXRATE_5 0.32
+#define TAXRATE_6 0.35
+#define TAXRATE_7 0.37
+
 int main(void) 
 {
 
@@ -9,6 +16,12 @@ float cash_income;
 printf("What is your total compensation in US$: ");
 scanf("%f", &cash_income);
 
+if (cash_income >= 11925); 
+float tax_amount1 = 11925 * TAXRATE_1;
 
 
 }
+
+
+
+
