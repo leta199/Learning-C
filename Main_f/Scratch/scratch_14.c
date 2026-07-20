a1ad2d523708319ed15fc0bd054c4b5a492619fd
“leta199” <215572592+leta199@users.noreply.github.com>
2026-07-19 20:57:13 -0400

    Let us see how increenet and decrement work

--- Main_f/Scratch/scratch_14.c
+++ Main_f/Scratch/scratch_14.c
@@ -1,2 +1,14 @@
 // Just checking how pre and psot fix operators work
 
+int main(void) 
+{
+int i = 10;
+int m = 2;
+
+while (i > m){
+printf("Value of pre-increment %d", ++i);
+printf("Value of pre-increment %d", ++i);
+}
+
+return 0;
+}
