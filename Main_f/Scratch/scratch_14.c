43183abb7aac69beacdc9672867409c583e3c437
“leta199” <215572592+leta199@users.noreply.github.com>
2026-07-19 21:12:40 -0400

    Changed decrement to be in print

--- Main_f/Scratch/scratch_14.c
+++ Main_f/Scratch/scratch_14.c
@@ -9,10 +9,8 @@ int j = 10;
 int m = 2;
 
 while (i > m && j > m){
---i;
-j--;
-printf("Value of pre-decrement i is: %d\t", i);
-printf("Value of post-decrement j is: %d\n", j);
+printf("Value of pre-decrement i is: %d\t", --i);
+printf("Value of post-decrement j is: %d\n", j--);
 }
 
 return 0;
