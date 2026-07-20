fd45fd1cff14ab00a1ea9ee0cfc9322d8f2b76bd
“leta199” <215572592+leta199@users.noreply.github.com>
2026-07-19 20:42:34 -0400

    Done with scratch_13 logic

--- Main_f/Scratch/scratch_13.c
+++ Main_f/Scratch/scratch_13.c
@@ -8,6 +8,17 @@ int i = 1;
 int j = 0;
 int n;
 
-printf("")
+printf("What value of n would you like?:  ");
+scanf("%d", &n);
+
+while (i < n) 
+{
+i *= 2;
+++j;
+}
+
+printf("Two must be raised to %d to be greater than or equal to %d", j, n);
+
+return 0;
 
 }
