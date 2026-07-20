796b6c5d81e1262c5cbecb931ab358632002aac2
“leta199” <215572592+leta199@users.noreply.github.com>
2026-07-19 21:09:02 -0400

    More fixed to decrement remember ++i not i = ++i

--- Main_f/Scratch/scratch_14.c
+++ Main_f/Scratch/scratch_14.c
@@ -9,8 +9,8 @@ int j = 10;
 int m = 2;
 
 while (i > m && j > m){
-i = --i;
-j = j--;
+--i;
+j--;
 printf("Value of pre-decrement i is: %d\t", i);
 printf("Value of post-decrement j is: %d\n", j);
 }
