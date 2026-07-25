1330f8e5fa193147bb6c1894012166c16582b8e0
“leta199” <215572592+leta199@users.noreply.github.com>
2026-07-24 21:58:46 -0400

    Fixed error in expr 2 in for loop

--- Main_f/Scratch/scratch_16.c
+++ Main_f/Scratch/scratch_16.c
@@ -4,9 +4,9 @@
 
 int main(void)
 {
-        
-for (int i = 10; i <= 0; i--)
-printf("T minus: %d seconds\n",i);
+
+for (int i = 10; i >= 0; i--)
+printf("T minus: %d seconds\n", i);
 
 return 0;
 }
