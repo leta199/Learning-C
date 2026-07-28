f2d59164c11a1b4eda33ae1c9df2dd1ee9c85844
“leta199” <215572592+leta199@users.noreply.github.com>
2026-07-27 23:24:07 -0400

    Done

--- Main_f/Scratch/scratch_16.c
+++ Main_f/Scratch/scratch_16.c
@@ -5,7 +5,7 @@
 int main(void)
 {
 
-for (int i = 10; i >= 0; i--)
+for (int i = 10; i >= 0; i--) /* Old versions of C needed decalartion of variable outside for loop*/
 printf("T minus: %2d seconds\n", i);
 
 return 0;
