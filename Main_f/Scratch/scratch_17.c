295a46719d57ce36b83e63712d207941a1464b52
“leta199” <215572592+leta199@users.noreply.github.com>
2026-07-30 23:15:39 -0400

    Didn;t wokr so lets rewrite the loop

--- Main_f/Scratch/scratch_17.c
+++ Main_f/Scratch/scratch_17.c
@@ -4,19 +4,15 @@
 int main(void){
 
 int n;
-int i;
+int i = 9;
 int j = 0;
 printf("Enter you integer value: \n");
 scanf("%d", &n);
 
-for (i = 9; i <= j; i--){
-   if (n % i == 0){
+for (; i <= j; i--)
+   if (n % i == 0)
     break;
-    printf("%d is divisble by %d\n", n, i);
-   }
-   else;
-   printf("%d is not divisible by 1 - 9", n);
-}
+    printf("%d is divisble by %d\n", n, i);  
 
 return 0;
 }
