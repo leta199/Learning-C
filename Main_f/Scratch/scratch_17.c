4d958d5b4f2c99806081c7637b84b52ea347d814
“leta199” <215572592+leta199@users.noreply.github.com>
2026-07-30 23:01:14 -0400

    Doing loop logic

--- Main_f/Scratch/scratch_17.c
+++ Main_f/Scratch/scratch_17.c
@@ -5,11 +5,14 @@ int main(void){
 
 float n;
 int i;
+int j = 0;
 printf("Enter you value: \n");
 scanf("%f", &n);
 
-
-
-
-
+for (i = 9; i <= j; i--){
+   if (n % i == 0){
+    break;
+    printf("%f is divisble by %d\n", n, i);
+   }
+}
 }
