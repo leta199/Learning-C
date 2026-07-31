3dfede94f7671cde3f406581e1c3bbe81dd08808
“leta199” <215572592+leta199@users.noreply.github.com>
2026-07-30 23:07:54 -0400

    Fixed format specifier

--- Main_f/Scratch/scratch_17.c
+++ Main_f/Scratch/scratch_17.c
@@ -7,15 +7,15 @@ int n;
 int i;
 int j = 0;
 printf("Enter you integer value: \n");
-scanf("%f", &n);
+scanf("%d", &n);
 
 for (i = 9; i <= j; i--){
    if (n % i == 0){
     break;
-    printf("%f is divisble by %d\n", n, i);
+    printf("%d is divisble by %d\n", n, i);
    }
    else;
-   printf("%f is not divisible by 1 - 9", n);
+   printf("%d is not divisible by 1 - 9", n);
 }
 
 return 0;
