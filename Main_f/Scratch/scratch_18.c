fd0c0ddfb369ac2ce49a6fe4f9549736cb31f0b2
“leta199” <215572592+leta199@users.noreply.github.com>
2026-08-02 21:43:56 -0400

    Implementing while loop

--- Main_f/Scratch/scratch_18.c
+++ Main_f/Scratch/scratch_18.c
@@ -4,9 +4,12 @@
 
 int main(void) 
 {
-int a,b,c,d,e,f,g,h,i,j:
-printf("Please enter 10 digits ending in zero: ")
-scanf("%d%d%d%d%d%d%d%d%d%d", )
+int a,b,c,d,e,f,g,h,i,j;
+printf("Please enter 10 non zero digits ending in zero: ");
+
+while (n < 10 )
+scanf("%d%d%d%d%d%d%d%d%d%d", &a,&b,&c,&d,&e,&f,&g,&h,&i,&j);
+
 
 
 }
