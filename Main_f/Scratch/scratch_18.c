1ceff48e6a0a606629f2861cbe2cf577542d78a4
“leta199” <215572592+leta199@users.noreply.github.com>
2026-08-02 21:51:29 -0400

    Done and now test

--- Main_f/Scratch/scratch_18.c
+++ Main_f/Scratch/scratch_18.c
@@ -8,8 +8,15 @@ int n, sum, i;
 printf("Please enter 10 non zero digits ending in zero: ");
 
 while (n < 10){
-scanf("%d", &a);
-
-
+    scanf("%d", &i);
+    if (i == 0)
+    continue;
+    sum += i
+    n++
 }
+
+printf("The sum is %d\n", sum);
+
+return 0;
+
 }
